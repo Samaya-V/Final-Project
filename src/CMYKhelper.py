46013e3985b41ea9f7460e45197a999af4b93a2d
Samaya V <svaideswar@gmail.com>
2026-04-30 20:57:27 -0500

    began CMYK splitting custom functions

--- src/CMYKhelper.py
+++ src/CMYKhelper.py
@@ -1,6 +1,18 @@
 from tkinter import *
-import PIL
+from tkinter import ttk
+from PIL import Image
 
-window = Tk # i made a window
+def split_image_into_channels(img, img_name):
+    cyan, magenta, yellow, key = img.split()
+    
 
-window.mainloop # i can open it now. im scared.
+
+def convert_RGB_to_CMYK(img):
+    img_CMYK = img.convert("CMYK")
+    return img_CMYK
+
+def main():
+    window = Tk() # i made a window
+    window.geometry("800x500")
+    window.title("CMYK Helper :D")
+    window.mainloop() # i can open it now. im scared.
