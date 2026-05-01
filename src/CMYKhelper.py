9b29e01ebc8ebd4fc7a00529bf9238a972f41b13
Samaya V <svaideswar@gmail.com>
2026-04-30 21:07:34 -0500

    finished conversion and splitting function, created main loop.

--- src/CMYKhelper.py
+++ src/CMYKhelper.py
@@ -2,13 +2,18 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image
 
-def split_image_into_channels(img, img_name):
-    cyan, magenta, yellow, key = img.split()
-    
-
-
-def convert_RGB_to_CMYK(img):
+def get_split_image_into_channels(img, img_name):
     img_CMYK = img.convert("CMYK")
+    cyan, magenta, yellow, key = img_CMYK.split()
+    cyan.save(f"{img_name}_cyan.pdf", resolution=300.0)
+    magenta.save(f"{img_name}_magenta.pdf)", resolution=300.0)
+    yellow.save(f"{img_name}_yellow.pdf)", resolution=300.0)
+    key.save(f"{img_name}_key.pdf)", resolution=300.0)
+
+
+def convert_RGB_to_CMYK(img, img_name):
+    img_CMYK = img.convert("CMYK")
+    img_CMYK.save(f"{img_name}_CMYK.pdf", resolution=300.0)
     return img_CMYK
 
 def main():
@@ -16,3 +21,6 @@ def main():
     window.geometry("800x500")
     window.title("CMYK Helper :D")
     window.mainloop() # i can open it now. im scared.
+
+if __name__ == "__main__":
+    main()
